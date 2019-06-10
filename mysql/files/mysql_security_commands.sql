6b9f331fd9c0116dd3ea616d8c8f45f6e0f6f8fa
root <root@social.np.prombez>
2019-06-10 15:13:11 +0300

    Ubuntu-based mysql ISO setup 1.0

--- mysql/files/mysql_security_commands.sql
+++
@@ -1,9 +0,0 @@
-# This set of commands will modify the predefined accounts of a MySQL installation
-# to increase security.
-
-SET @@old_passwords=0; 
-UPDATE mysql.user SET Password=PASSWORD('123456') WHERE User='root' and plugin in ('', 'mysql_native_password');
-SET @@old_passwords=2; 
-UPDATE mysql.user SET authentication_string=PASSWORD('123456') WHERE User='root' and plugin='sha256_password'; 
-
-FLUSH PRIVILEGES;
